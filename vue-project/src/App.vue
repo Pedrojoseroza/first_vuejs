16f7d6dfb3d219a6b290c7965192fb36b73a85c7
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2025-12-02 22:06:48 -0300

    First Message

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++
+    },
+    decrementarContador() {
+      this.contador--
+    }
+  },
+  mounted() {
+    console.log(`O valor inicial do contador é ${this.contador}.`)
+  }
+} // Será apresentado a seguir
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
